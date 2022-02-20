6e981c58dc7e78115fa1e7a55d8cd809f247a293
dennysferrer <dennys.ferrer@gmail.com>
2022-02-20 15:54:10 -0500

    Se trabajo sobre el componente de saludo y se estudio la forma de escribir directamente en el html del componente a través de las variables creadas en el archivo ts

--- MiPrimeraApp-Angular/src/app/saludo/saludo.component.ts
+++ MiPrimeraApp-Angular/src/app/saludo/saludo.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SaludoComponent implements OnInit {
 
-  constructor() { }
+  mensaje: string; 
+  identiParrafo: string;
+  identiInput: string;
+
+  constructor() {
+    this.mensaje = 'Un saludo a todos...';
+    this.identiParrafo = 'parrafoPrincipal';
+    this.identiInput = 'text';
+
+    setTimeout(() => {
+      this.mensaje = 'Hola mundo ...';
+      this.identiParrafo = 'main';
+      this.identiInput = 'password';
+    }, 5000);
+  }
 
   ngOnInit(): void {
   }
+
+  mostrarSaludo(): string {
+    return 'Saludo desde el método...';
+  }
+
+  
 
 }
